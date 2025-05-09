b142759eb5220ab15466d4f843be769905e29c03
Kotaro-Yamagishi <koutannn772@gmail.com>
2025-05-10 01:01:08 +0900

    add cooment for decorator

--- src/main/java/com/headfirst/designpattern/decorator/CondimentDecorator.java
+++ src/main/java/com/headfirst/designpattern/decorator/CondimentDecorator.java
@@ -2,6 +2,7 @@ package com.headfirst.designpattern.decorator;
 
 public abstract class CondimentDecorator extends Beverage {
     Beverage beverage;
+    // このクラスを継承したDecoratorは再度getDescription()をオーバーライドする必要がある
     public abstract String getDescription();
     
 }
